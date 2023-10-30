3f40f42226589dbefb84575d4664d67b2ce41c5c
Meagan <MeaganTruong1@yahoo.com>
2023-10-30 18:34:07 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.round(Math.random());
+
+let num3 = 32;
+let num4 = 28;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
